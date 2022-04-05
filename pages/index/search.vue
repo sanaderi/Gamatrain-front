bf54e97e8c197c903c0472720751e9620d49ebf5
seyedehhelma hosseini <inosoft.ino0204@gmail.com>
2022-04-05 10:45:49 +0430

    create grade-card

---
+++ pages/index/search.vue
@@ -0,0 +1,56 @@
+<template>
+  <form @submit.prevent="submit">
+    <v-row class="align-center">
+      <v-col cols="12" md="3">
+        <p class="mb-2">مقطع</p>
+        <v-autocomplete
+          v-model="values"
+          :items="items"
+          chips
+          dense
+          outlined
+          small-chips
+        ></v-autocomplete>
+      </v-col>
+      <v-col cols="12" md="3">
+        <p class="mb-2">پایه</p>
+        <v-autocomplete
+          v-model="values"
+          :items="items"
+          chips
+          dense
+          outlined
+          small-chips
+        ></v-autocomplete>
+      </v-col>
+      <v-col cols="12" md="3">
+        <p class="mb-2">کتاب درسی</p>
+        <v-autocomplete
+          v-model="values"
+          :items="items"
+          chips
+          dense
+          outlined
+          small-chips
+        ></v-autocomplete>
+      </v-col>
+      <v-col class="d-flex justify-end" cols="12" md="3">
+        <v-btn class="search-btn" depressed>
+          جستجو
+        </v-btn>
+      </v-col>
+    </v-row>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: ['همه', 'دبستان', 'متوسطه'],
+      values: ['همه'],
+      value: null,
+    }
+  }
+}
+</script>
