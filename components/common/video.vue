703fc8b594f5326990f5e416a05284a9a1e8dac8
seyedehhelma hosseini <inosoft.ino0204@gmail.com>
2022-07-06 09:18:53 +0430

    edit player in lesson page

--- components/common/video.vue
+++ components/common/video.vue
@@ -1,52 +1,88 @@
 <template>
-        <v-carousel :show-arrows="false">
-            <v-carousel-item class="item-video" v-for="item in videos" :key="item.value">
-                <video id="my-player" class="video-js video-carousel" controls
-                    :poster="require('@/assets/images/' + item.img)" data-setup='{}'>
-                    <source :src="require('@/assets/video/' + item.vid)" type="video/mp4" />
-                </video>
-                <div class="shadow"></div>
-            </v-carousel-item>
-        </v-carousel>
+  <!-- <v-carousel :show-arrows="false">
+      <v-carousel-item class="item-video" v-for="item in videos" :key="item.value">
+          <video id="my-player" class="video-js video-carousel" controls
+              :poster="require('@/assets/images/' + item.img)" data-setup='{}'>
+              <source :src="require('@/assets/video/' + item.vid)" type="video/mp4" />
+          </video>
+          <div class="shadow"></div>
+      </v-carousel-item>
+  </v-carousel> -->
+  <v-carousel :show-arrows="false">
+    <v-carousel-item v-for="(item, index) in videos" :key="index">
+      <div class="video-player">
+        <video
+          :id="'video' + index"
+          :poster="require('@/assets/images/' + item.img)"
+          :src="require('assets/video/' + item.vid)"
+          class="video"
+          width="100%">
+        </video>
+        <div class="buttons">
+          <button
+            :id="'pausePlay' + index"
+            class="play"
+            @click="play(index)">
+          </button>
+        </div>
+        <div class="shadow"></div>
+      </div>
+    </v-carousel-item>
+  </v-carousel>
 </template>
 <script>
 // import VideoPlayer from "nuxt-video-player";
 // require("nuxt-video-player/src/assets/css/main.css");
 export default {
-    data() {
-        return {
-            rating: 4.5,
-            videos: [
-                {
-                    img: "vid.png",
-                    vid: "video.mp4"
-                },
-                {
-                    img: "vid.png",
-                    vid: "video.mp4"
-                },
-                {
-                    img: "vid.png",
-                    vid: "video.mp4"
-                },
-                {
-                    img: "vid.png",
-                    vid: "video.mp4"
-                },
-                {
-                    img: "vid.png",
-                    vid: "video.mp4"
-                },
-                {
-                    img: "vid.png",
-                    vid: "video.mp4"
-                },
+  data() {
+    return {
+      video: "video.mp4",
+      img: "vid.png",
+      videos: [
+        {
+          img: "vid.png",
+          vid: "video.mp4"
+        },
+        {
+          img: "vid.png",
+          vid: "video.mp4"
+        },
+        {
+          img: "vid.png",
+          vid: "video.mp4"
+        },
+        {
+          img: "vid.png",
+          vid: "video.mp4"
+        },
+        {
+          img: "vid.png",
+          vid: "video.mp4"
+        },
+        {
+          img: "vid.png",
+          vid: "video.mp4"
+        },
 
-            ]
-        }
-    },
-    components: {
-        // VideoPlayer,
-    },
+      ]
+    }
+  },
+  components: {
+    // VideoPlayer,
+  },
+  methods: {
+    play(index) {
+      let btn = document.getElementById('pausePlay'+ index)
+      let vid = document.getElementById('video'+ index)
+
+      if (vid.paused) {
+        btn.className = "pause"
+        vid.play();
+      } else {
+        btn.className = "play";
+        vid.pause();
+      }
+    }
+  },
 };
 </script>
