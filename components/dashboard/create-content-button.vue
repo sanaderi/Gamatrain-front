e4e7e1c594142667ccc9b472bc66ccdc7e551f17
Sadeq Naderi <sadeq.naderi@gmail.com>
2022-09-21 13:29:20 +0430

    Enable Teacehr dashboard API

--- components/dashboard/create-content-button.vue
+++ components/dashboard/create-content-button.vue
@@ -1,10 +1,12 @@
 <template>
   <!--Content type-->
   <v-row class="create_content_section">
-    <v-col cols="6" md="3" v-for="(item,index) in button_list" :class="`${item.class}`">
+    <v-col cols="6" md="3"
+           class="px-2"
+           v-for="(item,index) in button_list" :class="`${item.class}`">
       <v-card outlined :disabled="$auth.user.group_id==='6' && button_list[index].class!=='question_answer'">
-        <v-text>
-          <v-row class="pa-6">
+        <v-card-text class="px-0 px-md-4">
+          <v-row >
             <v-col cols="6" class="text-left">
               <v-btn class="icon_btn" fab x-large>
                 <span :class="`icon ${item.icon} group-icon`"></span>
@@ -17,7 +19,7 @@
                 {{item.count}}
               </span>
             </v-col>
-            <v-col cols="12" class="title">
+            <v-col cols="12" class="title py-0">
               {{item.title}}
             </v-col>
             <v-col cols="12">
@@ -27,7 +29,7 @@
             </v-col>
           </v-row>
 
-        </v-text>
+        </v-card-text>
       </v-card>
     </v-col>
   </v-row>
--- components/dashboard/question-statistics.vue
+++ components/dashboard/question-statistics.vue
@@ -1,56 +1,36 @@
 <template>
-  <div class="d-inline-flex " >
+  <div class="d-inline-flex ">
     <v-carousel height="60" hide-delimiters cycle :show-arrows="false">
       <v-carousel-item>
-        <div class="px-3 text-center question-section">
-          <v-btn
-            color="#FF9800"
-            dark
-            fab
-            x-small
-          >
+        <div class="px-3 text-center question">
+
            <span class="counter">
-               {{statistics.question && statistics.question.total ? statistics.question.total : 0}}
+               {{ statistics.question && statistics.question.total ? statistics.question.total : 0 }}
           </span>
-          </v-btn>
 
 
-          <p class="text-h6 mt-1 ">
+          <p class="text-h6  ">
             Question's
           </p>
 
         </div>
       </v-carousel-item>
       <v-carousel-item>
-        <div class="px-3 text-center">
-          <v-btn
-            color="#E2CA31"
-            dark
-            x-small
-            fab
-          >
+        <div class="px-3 text-center question-inbox">
         <span class="counter">
-          {{statistics.question && statistics.question.unreadReplies ? statistics.question.unreadReplies : 0}}
+          {{ statistics.question && statistics.question.unreadReplies ? statistics.question.unreadReplies : 0 }}
         </span>
-          </v-btn>
-          <p class="text-h6 mt-1 " >
+          <p class="text-h6 ">
             Inbox
           </p>
         </div>
       </v-carousel-item>
       <v-carousel-item>
-        <div class="px-3 text-center question-section">
-          <v-btn
-            color="#00D2AE"
-            dark
-            x-small
-            fab
-          >
-        <span class="counter">
-             {{statistics.questionReply && statistics.questionReply.total ? statistics.questionReply.total : 0}}
-        </span>
-          </v-btn>
-          <p class="text-h6 mt-1 font-weight-bold ">
+        <div class="px-3 text-center question-outbox">
+          <span class="counter ">
+             {{ statistics.questionReply && statistics.questionReply.total ? statistics.questionReply.total : 0 }}
+          </span>
+          <p class="text-h6 ">
             Outbox
           </p>
 
@@ -58,69 +38,69 @@
       </v-carousel-item>
     </v-carousel>
 
-<!--    <div class="pl-0 pr-8 text-center ">-->
-<!--       <span class="icon icong-qa group-icon white&#45;&#45;text"></span>-->
+    <!--    <div class="pl-0 pr-8 text-center ">-->
+    <!--       <span class="icon icong-qa group-icon white&#45;&#45;text"></span>-->
 
-<!--      <p class="text-h6 mt-1 white&#45;&#45;text">-->
-<!--        Q & A-->
-<!--      </p>-->
+    <!--      <p class="text-h6 mt-1 white&#45;&#45;text">-->
+    <!--        Q & A-->
+    <!--      </p>-->
 
-<!--    </div>-->
-<!--    <div class="px-3 text-center question-section">-->
-<!--      <v-btn-->
-<!--        color="#FF9800"-->
-<!--        dark-->
-<!--        fab-->
-<!--      >-->
-<!--           <span class="font-weight-bold">-->
-<!--               {{statistics.question && statistics.question.total ? statistics.question.total : 0}}-->
-<!--          </span>-->
-<!--      </v-btn>-->
+    <!--    </div>-->
+    <!--    <div class="px-3 text-center question-section">-->
+    <!--      <v-btn-->
+    <!--        color="#FF9800"-->
+    <!--        dark-->
+    <!--        fab-->
+    <!--      >-->
+    <!--           <span class="font-weight-bold">-->
+    <!--               {{statistics.question && statistics.question.total ? statistics.question.total : 0}}-->
+    <!--          </span>-->
+    <!--      </v-btn>-->
 
 
-<!--      <p class="text-h6 mt-1 white&#45;&#45;text">-->
-<!--        Question's-->
-<!--      </p>-->
+    <!--      <p class="text-h6 mt-1 white&#45;&#45;text">-->
+    <!--        Question's-->
+    <!--      </p>-->
 
-<!--    </div>-->
-<!--    <div class="px-3 text-center">-->
-<!--      <v-btn-->
-<!--        color="#E2CA31"-->
-<!--        dark-->
-<!--        fab-->
-<!--      >-->
-<!--        <span class="font-weight-bold">-->
-<!--          {{statistics.question && statistics.question.unreadReplies ? statistics.question.unreadReplies : 0}}-->
-<!--        </span>-->
-<!--      </v-btn>-->
-<!--      <p class="text-h6 mt-1 white&#45;&#45;text" >-->
-<!--        Inbox-->
-<!--      </p>-->
-<!--    </div>-->
-<!--    <div class="px-3 text-center question-section">-->
-<!--      <v-btn-->
-<!--        color="#00D2AE"-->
-<!--        dark-->
-<!--        fab-->
-<!--      >-->
-<!--        <span class="font-weight-bold white&#45;&#45;text">-->
-<!--             {{statistics.questionReply && statistics.questionReply.total ? statistics.questionReply.total : 0}}-->
-<!--        </span>-->
-<!--      </v-btn>-->
-<!--      <p class="text-h6 mt-1 font-weight-bold white&#45;&#45;text">-->
-<!--        Outbox-->
-<!--      </p>-->
+    <!--    </div>-->
+    <!--    <div class="px-3 text-center">-->
+    <!--      <v-btn-->
+    <!--        color="#E2CA31"-->
+    <!--        dark-->
+    <!--        fab-->
+    <!--      >-->
+    <!--        <span class="font-weight-bold">-->
+    <!--          {{statistics.question && statistics.question.unreadReplies ? statistics.question.unreadReplies : 0}}-->
+    <!--        </span>-->
+    <!--      </v-btn>-->
+    <!--      <p class="text-h6 mt-1 white&#45;&#45;text" >-->
+    <!--        Inbox-->
+    <!--      </p>-->
+    <!--    </div>-->
+    <!--    <div class="px-3 text-center question-section">-->
+    <!--      <v-btn-->
+    <!--        color="#00D2AE"-->
+    <!--        dark-->
+    <!--        fab-->
+    <!--      >-->
+    <!--        <span class="font-weight-bold white&#45;&#45;text">-->
+    <!--             {{statistics.questionReply && statistics.questionReply.total ? statistics.questionReply.total : 0}}-->
+    <!--        </span>-->
+    <!--      </v-btn>-->
+    <!--      <p class="text-h6 mt-1 font-weight-bold white&#45;&#45;text">-->
+    <!--        Outbox-->
+    <!--      </p>-->
 
-<!--    </div>-->
+    <!--    </div>-->
   </div>
 </template>
 
 <script>
 export default {
   name: "question-statistics",
-  data(){
-    return{
-      statistics:''
+  data() {
+    return {
+      statistics: ''
     }
   }
 }
@@ -129,14 +109,21 @@ export default {
 <style scoped>
 
 
-.group-icon{
+.group-icon {
   font-size: 5rem;
 }
 
-.counter{
-  font-style: normal;
-  font-weight: 400;
-  font-size: 1.2rem;
-  line-height: 30px;
+
+
+.question {
+  color: #FF9800
+}
+
+.question-inbox {
+  color: orchid;
+}
+
+.question-outbox {
+  color: #00D2AE
 }
 </style>
