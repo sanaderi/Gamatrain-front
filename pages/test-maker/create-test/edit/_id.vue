221ee18a9e4351b629e7117c90429d207b62e201
Sadeq Naderi <sadeq.naderi@gmail.com>
2022-11-28 19:38:45 +0330

    Enable edit examTests and arbiter

---
+++ pages/test-maker/create-test/edit/_id.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container class="pb-10 mb-16">
+    <create-test-form/>
+  </v-container>
+</template>
+
+<script>
+import CreateTestForm from "@/components/test-maker/create-test-form";
+
+export default {
+  layout: "test-maker-layout",
+  name: "test-maker",
+  head() {
+    return {
+      title: 'Create online exam',
+      correct_answer: 1,
+      script: [
+        //MathType to Latex
+        // {
+        //   type: 'text/javascript',
+        //   src: '/MathType.js'
+        // },
+        // {
+        //   type: 'text/javascript',
+        //   src: 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=MML_HTMLorMML',
+        //   async: true,
+        // }
+        //End MathType to Latex
+      ]
+    }
+  },
+  components: {
+    CreateTestForm,
+  },
+
+}
+</script>
