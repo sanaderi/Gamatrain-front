f0bc508fe84cac064462dcb17ef9143ede66796d
Sadeq Naderi <sadeq.naderi@gmail.com>
2023-06-20 16:13:24 +0330

    Update nuxt version

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,9 +26,15 @@ export default {
       {rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=PT+Sans&display=swap"},
     ],
     script: [
-      {src: 'https://accounts.google.com/gsi/client',defer:true,async:true},
+      {src: 'https://accounts.google.com/gsi/client', defer: true, async: true},
       // {src: './assets/js/jquery.js', body: true},
       // {src: './assets/js/video.min.js', body: true},
+    ],
+  },
+
+  generate: {
+    routes: [
+      '/'
     ],
   },
 
@@ -44,7 +50,6 @@ export default {
     {src: 'plugins/vee-validate.js', ssr: true},
     {src: 'plugins/axios.js'},
     {src: 'plugins/helper.js'},
-    {src: 'plugins/vuex-persist', ssr: false},
     {src: 'plugins/vue-emoji-picker', ssr: false},
     {src: 'plugins/img-cropper', ssr: false},
     {src: 'plugins/vuedraggable', ssr: false},
@@ -63,6 +68,7 @@ export default {
     '@nuxtjs/dotenv',
     '@nuxtjs/moment',
     '@nuxtjs/pwa',
+
     // '@nuxtjs/onesignal',
   ],
 
@@ -89,7 +95,7 @@ export default {
 
   axios: {
     proxy: false,
-    baseURL:process.env.API_BASE_URL,
+    baseURL: process.env.API_BASE_URL,
     headers: {}
   },
 
@@ -167,7 +173,7 @@ export default {
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    defaultAssets:false,
+    defaultAssets: false,
     customVariables: ["~/assets/variables.scss"],
     theme: {
       dark: false,
@@ -190,14 +196,14 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    transpile: ["vee-validate","vue-chartjs"],
+    transpile: ["vee-validate", "vue-chartjs"],
   },
 
   pwa: {
     manifest: {
       name: 'Gamatrain App',
-      short_name:'Gamatrain',
-      description:'Big training platform',
+      short_name: 'Gamatrain',
+      description: 'Big training platform',
       lang: 'en',
       useWebmanifestExtension: false
     }
