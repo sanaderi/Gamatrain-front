9e7025c2c5110616f85660cad731b6c6792b5705
Sadeq Naderi <sadeq.naderi@gmail.com>
2023-09-23 13:07:50 +0330

    Enbale home blog list

--- components/home/blog-container.vue
+++ components/home/blog-container.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container id="blog-list-container">
-        <v-card flat  >
+        <v-card flat>
             <v-card-text class="px-0 px-sm-6 main-card">
                 <v-row>
                     <v-col cols="6">
@@ -15,14 +15,18 @@
                     </v-col>
                     <v-col cols="12">
                         <v-slide-group v-model="model" class="slider py-sm-4" :show-arrows="$vuetify.breakpoint.lgAndUp">
-                            <v-slide-item v-for="(item, n) in slideItmes" :key="n" >
-                                <v-card flat>
+                            <v-slide-item v-if="isLoading" v-for="i in 10">
+                                <v-skeleton-loader class="mx-auto slide-loading"  type="card"></v-skeleton-loader>
+                            </v-slide-item>
+                            
+                            <v-slide-item v-else v-for="(item, n) in slideItmes" :key="n">
+                                <v-card flat :to="`/blog/${item.id}/${item.title}`">
                                     <v-card @mouseover="toggleHover('enter', n)" @mouseleave="toggleHover('leave', n)"
                                         class="ma-1">
-                                        <v-img :src="`/images/${item.img}`" />
-                                        <v-card-title class="text-white">
-                                            <span v-if="!isHovered[n]">
-                                                {{ item.short_describe }}
+                                        <v-img :src="item.pic" />
+                                        <v-card-title>
+                                            <span class="gama-text-button" v-if="!isHovered[n]">
+                                                {{ item.title }}
                                             </span>
                                             <div v-else class="text-center">
                                                 <v-btn text size="small" color="primary">
@@ -30,9 +34,9 @@
                                             </div>
                                         </v-card-title>
                                     </v-card>
-                                    <div class="describe">
-                                        we have a few fundamentalwe principles that ...
-                                        <nuxt-link to="/">Read more</nuxt-link>
+                                    <div class="gama-text-subtitle2">
+                                        <span v-html="item.body"></span>
+                                        <nuxt-link :to="`/blog/${item.id}/${item.title}`">Read more</nuxt-link>
                                     </div>
                                 </v-card>
                             </v-slide-item>
@@ -52,49 +56,12 @@ export default {
         return {
             model: null,
             isHovered: [],
-            slideItmes: [
-                {
-                    img: 'blog1.png',
-                    short_describe: 'Excited young teachercherr eachercherr wearing..',
-                    readmore_describe: 'Excited young teachercherr eachercherr wearing..'
-                },
-                {
-                    img: 'blog2.png',
-                    short_describe: 'Excited young teachercherr eachercherr wearing..',
-                    readmore_describe: 'Excited young teachercherr eachercherr wearing..'
-                },
-                {
-                    img: 'blog1.png',
-                    short_describe: 'Excited young teachercherr eachercherr wearing..',
-                    readmore_describe: 'Excited young teachercherr eachercherr wearing..'
-                },
-                {
-                    img: 'blog2.png',
-                    short_describe: 'Excited young teachercherr eachercherr wearing..',
-                    readmore_describe: 'Excited young teachercherr eachercherr wearing..'
-                },
-                {
-                    img: 'blog1.png',
-                    short_describe: 'Excited young teachercherr eachercherr wearing..',
-                    readmore_describe: 'Excited young teachercherr eachercherr wearing..'
-                },
-                {
-                    img: 'blog1.png',
-                    short_describe: 'Excited young teachercherr eachercherr wearing..',
-                    readmore_describe: 'Excited young teachercherr eachercherr wearing..'
-                },
-                {
-                    img: 'blog1.png',
-                    short_describe: 'Excited young teachercherr eachercherr wearing..',
-                    readmore_describe: 'Excited young teachercherr eachercherr wearing..'
-                },
-                {
-                    img: 'blog1.png',
-                    short_describe: 'Excited young teachercherr eachercherr wearing..',
-                    readmore_describe: 'Excited young teachercherr eachercherr wearing..'
-                }
-            ]
+            slideItmes: [],
+            isLoading:false
         }
+    },
+    mounted() {
+        this.loadBlog();
     },
     methods: {
         toggleHover(action, n) {
@@ -104,6 +71,17 @@ export default {
                 this.isHovered[n] = false;
 
 
+        },
+        loadBlog() {
+            this.isLoading=true;
+            this.$axios.$get('/api/v1/home/news')
+                .then(response => {
+                    this.slideItmes = response.data;
+                }).catch(err => {
+                    console.log(err);
+                }).finally(()=>{
+                    this.isLoading=false;
+                })
         }
     }
 }
@@ -113,6 +91,7 @@ export default {
 <style>
 #blog-list-container {
     margin: 2.4rem auto 2.4rem auto;
+    min-height: 40rem;
 
     #main-title {
         color: #354053;
@@ -153,6 +132,12 @@ export default {
     }
 
     .slider {
+        .slide-loading{
+            width: 21.9rem;
+            margin-right: 1.6rem!important;
+            position: relative;
+            border-radius: 0.6rem; 
+        }
         .v-card {
             width: 21.9rem;
             height: 19.9rem;
@@ -175,33 +160,33 @@ export default {
                     font-family: 'Inter-Meduim';
                     margin: 0 auto;
                     color: #FFF;
-                    font-size: 1.4rem;
-                    font-style: normal;
-                    font-weight: 600;
-                    line-height: 1.8rem;
                     padding-top: .6rem !important;
                     padding-bottom: .6rem;
+                    height: 4.9rem;
+                    max-height: 4.9rem;
 
-
+                    .gama-text-button {
+                        color: #FFF;
+                        overflow: hidden;
+                        text-overflow: ellipsis;
+                        display: -webkit-box;
+                        -webkit-line-clamp: 2;
+                        -webkit-box-orient: vertical;
+                    }
                 }
             }
         }
 
-        .describe {
-            text-align: center;
+        .gama-text-subtitle2 {
+            margin: auto;
+            max-width: 23rem;
+            text-align: left;
             color: #6E7781;
-            font-size: 1.2rem;
-            font-style: normal;
-            font-weight: 500;
-            line-height: 1.6rem;
 
 
-            .nuxt-link-exact-active {
+            &>a {
                 color: #FFB600;
-                font-size: 1.2rem;
-                font-style: normal;
-                font-weight: 500;
-                line-height: 1.6rem;
+
             }
         }
     }
@@ -212,8 +197,8 @@ export default {
     #blog-list-container {
 
         .main-card {
-            padding-left: 1.3rem!important;
-            padding-right: 1.3rem!important;
+            padding-left: 1.3rem !important;
+            padding-right: 1.3rem !important;
         }
 
         .slider {
@@ -234,28 +219,15 @@ export default {
                         padding-top: .8rem !important;
                         padding-bottom: .8rem;
                         line-height: normal;
+                        height: 5.4rem;
+                        max-height: 5.4rem;
 
 
                     }
                 }
             }
 
-            .describe {
-                color: #6E7781;
-                font-size: 1.4rem;
-                font-style: normal;
-                font-weight: 500;
-                line-height: normal;
 
-
-                .nuxt-link-exact-active {
-                    color: #FFB600;
-                    font-size: 1.4rem;
-                    font-style: normal;
-                    font-weight: 300;
-                    line-height: 2.4rem;
-                }
-            }
         }
     }
 }
@@ -267,8 +239,8 @@ export default {
 
 
         .main-card {
-            padding-left: 1.2rem!important;
-            padding-right: 1.2rem!important;
+            padding-left: 1.2rem !important;
+            padding-right: 1.2rem !important;
         }
 
         .slider {
@@ -297,22 +269,7 @@ export default {
                 }
             }
 
-            .describe {
-                color: #6E7781;
-                font-size: 1.4rem;
-                font-style: normal;
-                font-weight: 500;
-                line-height: normal;
 
-
-                .nuxt-link-exact-active {
-                    color: #FFB600;
-                    font-size: 1.4rem;
-                    font-style: normal;
-                    font-weight: 300;
-                    line-height: 2.4rem;
-                }
-            }
         }
     }
 }
