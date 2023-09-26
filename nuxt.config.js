546d5ab6baeadc3544d34b4f4b1b9b19609d955e
Sadeq Naderi <sadeq.naderi@gmail.com>
2023-09-26 23:15:09 +0330

    Load Inter font from google cdn

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,6 +20,8 @@ export default{
       {rel: "stylesheet", href: "/assets/css/fontawesome.min.css"},
       {rel: "icon", type: "image/x-icon", href: "/favicon.ico"},
       {rel: "stylesheet", href: "/assets/css/all.min.css"},
+      {rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"},
+      
     ],
     script: [
       {src: 'https://accounts.google.com/gsi/client', defer: true, async: true},
