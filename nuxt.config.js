538d69b64a06ed9bcb24490c55e313aba73e89a4
Sadeq Naderi <sadeq.naderi@gmail.com>
2023-09-27 12:18:01 +0330

    Change font source to locally serve

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ export default{
       {rel: "stylesheet", href: "/assets/css/fontawesome.min.css"},
       {rel: "icon", type: "image/x-icon", href: "/favicon.ico"},
       {rel: "stylesheet", href: "/assets/css/all.min.css"},
-      {rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"},
+      // {rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"},
       
     ],
     script: [
