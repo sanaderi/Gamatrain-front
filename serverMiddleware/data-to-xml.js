dff5e1d47359b68798249ff788f391412727b71e
Sadeq Naderi <sadeq.naderi@gmail.com>
2024-03-11 22:48:40 +0330

    Enable site map for paper, multimedia, exam, tutorial and update qa sitemap

--- serverMiddleware/data-to-xml.js
+++ serverMiddleware/data-to-xml.js
@@ -1,38 +1,74 @@
 import axios from 'axios';
 
 export default async (req, res, next) => {
-    if (req.url.startsWith('/sitemap')) {
-      // Example: Set content type to XML
-      res.setHeader('Content-Type', 'application/xml');
+  const { url } = req;
 
-      console.log("OK");
+  // Check if the request is for sitemap
+  if (url.startsWith('/sitemap')) {
+    // Example: Set content type to XML
+    res.setHeader('Content-Type', 'application/xml');
+    console.log("OK");
 
-  
-      // Replace this part with your actual data fetching logic
-      const data = await fetchDataFromServer();
-  
-      // Convert your data to XML format
-      const xmlData = convertDataToXML(data);
-  
-      // Send XML response
-      res.end(xmlData);
-    } else {
-      next();
+    // Extract the content type from the URL
+    const contentType = url.substring(url.lastIndexOf('/') + 1);
+
+
+    var xmlData="";
+    if (contentType!='sitemap') {
+      // Fetch data based on content type
+      const data = await fetchDataFromServer(contentType);
+
+      // Convert data to XML format
+      xmlData = convertDataToXML(data, contentType);
     }
-  };
-  
-  async function fetchDataFromServer() {
-    // Fetch paginated data from the third-party server
+    else{
+      xmlData=generateDefaultSitemap();
+    }
+
+    // Send XML response
+    res.end(xmlData);
+  } else {
+    next();
+  }
+};
+
+async function fetchDataFromServer(contentType) {
+   // Fetch paginated data from the third-party server
     const itemsPerPage = 50; // Adjust as per your pagination logic
     let currentPage = 1;
     let allData = [];
+    let apiUrl;
+
+
+
+     // Determine the API URL based on the content type
+  switch (contentType) {
+    case 'paper':
+      apiUrl = 'https://core.gamatrain.com/api/v1/search?type=test'; // Adjust API URL for papers
+      break;
+    case 'qa':
+      apiUrl = 'https://core.gamatrain.com/api/v1/search?type=question'; // Adjust API URL for papers
+      break;
+    case 'multimedia':
+      apiUrl = 'https://core.gamatrain.com/api/v1/search?type=learnfiles'; // Adjust API URL for schools
+      break;
+    case 'exam':
+      apiUrl = 'https://core.gamatrain.com/api/v1/search?type=azmoon'; // Adjust API URL for schools
+      break;
+    case 'tutorial':
+      apiUrl = 'https://core.gamatrain.com/api/v1/search?type=dars'; // Adjust API URL for schools
+      break;
+    default:
+      // Return empty array for unknown content type
+      return [];
+  }
 
 
 
     // Fetch data for all pages
     while (true) {
       const response = await axios.get(
-        `https://core.gamatrain.com/api/v1/search?type=question&page=${currentPage}&perpage=${itemsPerPage}`
+        `${apiUrl}&page=${currentPage}&perpage=${itemsPerPage}`
       );
       const data = response.data.data.list;
       console.log(data);
@@ -41,8 +77,8 @@ export default async (req, res, next) => {
       allData = [...allData, ...data];
 
       // Break the loop if no more data available
-      // if (data.length < itemsPerPage) {
-      if (currentPage > 20) {
+      if (data.length < itemsPerPage) {
+      // if (currentPage > 1000) {
         break;
       }
 
@@ -53,33 +89,59 @@ export default async (req, res, next) => {
 
 
     return allData;
-  }
-  
-  function convertDataToXML(data) {
-    let xml = '<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
-    xml+=`<url>
-    <loc>https://gamatrain.com/search?type=question</loc>
-    <lastmod>2023-11-14</lastmod>
-    <priority>1.00</priority>
-    </url>`;
-    data.forEach(item => { 
-      xml += `<url>
-          <loc>https://gamatrain.com/qa/${item.id}</loc>
-          <lastmod>${formatDate(item.up_date)}</lastmod>
-          <changefreq>weekly</changefreq>
-          <priority>0.8</priority>
+
+ 
+
+}
+
+function convertDataToXML(data, contentType) {
+  let xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+  data.forEach(item => {
+    xml += `<url>
+            <loc>https://gamatrain.com/${contentType}/${item.id}/${item.title.trim().replace(/ (?!$)/g, '-').toLowerCase()}</loc>
+            <lastmod>${formatDate(item.up_date)}</lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>0.8</priority>
+        </url>`;
+  });
+  xml += '\n</urlset>';
+  return xml;
+}
+
+
+
+function generateDefaultSitemap() {
+  console.log("tt");
+  const simpleUrls = [
+    'https://gamatrain.com',
+    'https://gamatrain.com/about-us',
+    'https://gamatrain.com/services',
+    'https://gamatrain.com/faq',
+    'https://gamatrain.com/search?type=test',
+    'https://gamatrain.com/search?type=learnfiles',
+    'https://gamatrain.com/search?type=question',
+    'https://gamatrain.com/search?type=azmoon',
+    'https://gamatrain.com/search?type=dars',
+    'https://gamatrain.com/blog',
+    'https://gamatrain.com/school',
+  ];
+
+  let xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+  simpleUrls.forEach(url => {
+    xml += `<url>
+          <loc>${url}</loc>
+          <changefreq>monthly</changefreq>
+          <priority>0.5</priority>
       </url>`;
-    });
-    xml += '\n</urlset>';
-    return xml;
-  }
+  });
+  xml += '\n</urlset>';
+  return xml;
+}
 
-  function delay(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
-
-// Function to format the date as YYYY-MM-DD
 function formatDate(dateString) {
   const date = new Date(dateString);
   const year = date.getFullYear();
@@ -87,4 +149,5 @@ function formatDate(dateString) {
   const day = String(date.getDate()).padStart(2, '0');
   return `${year}-${month}-${day}`;
 }
-  
+
+
