6b45626667a57486b799eb8818689eab6d4adcd7
Sadeq Naderi <sadeq.naderi@gmail.com>
2025-01-16 01:57:17 +0330

    Test load google client id

--- components/common/login.vue
+++ components/common/login.vue
@@ -1,9 +1,5 @@
 <template>
-  <v-dialog
-    v-model="login_dialog"
-    max-width="300px"
-    style="z-index: 20001"
-  >
+  <v-dialog v-model="login_dialog" max-width="300px" style="z-index: 20001">
     <v-card>
       <v-card-title>
         <span class="text-h5">Login</span>
@@ -11,20 +7,30 @@
       <v-card-text>
         <v-row>
           <v-col cols="12" class="text-center">
-            <v-divider class="my-2"/>
+            <v-divider class="my-2" />
             <div v-show="google_login_loading">
-              <v-progress-circular color="teal" class="mr-2" size="20" width="2" indeterminate>
+              <v-progress-circular
+                color="teal"
+                class="mr-2"
+                size="20"
+                width="2"
+                indeterminate
+              >
               </v-progress-circular>
               <span style="font-size: 1.2rem"> Loading google sign in</span>
             </div>
-            <div v-show="!google_login_loading" ref="googleLoginBtn"/>
+            <div v-show="!google_login_loading" ref="googleLoginBtn" />
           </v-col>
           <v-col cols="12">
             <validation-observer ref="observer" v-slot="{ invalid }">
               <form @submit.prevent="submit">
                 <v-row>
                   <v-col cols="12">
-                    <validation-provider v-slot="{ errors }" name="Email" rules="required">
+                    <validation-provider
+                      v-slot="{ errors }"
+                      name="Email"
+                      rules="required"
+                    >
                       <v-text-field
                         v-model="identity"
                         dense
@@ -36,7 +42,11 @@
                     </validation-provider>
                   </v-col>
                   <v-col cols="12">
-                    <validation-provider v-slot="{ errors }" name="Password" rules="required">
+                    <validation-provider
+                      v-slot="{ errors }"
+                      name="Password"
+                      rules="required"
+                    >
                       <v-text-field
                         label="Password"
                         v-model="password"
@@ -55,23 +65,28 @@
                     </p>
                   </v-col>
                   <v-col cols="12">
-                    <v-divider class="mb-3"/>
-                    <p class="text-h6 text-center pointer" @click="switchToRegister">
+                    <v-divider class="mb-3" />
+                    <p
+                      class="text-h6 text-center pointer"
+                      @click="switchToRegister"
+                    >
                       Not registered? register now
                     </p>
-                    <v-divider class="mt-3"/>
+                    <v-divider class="mt-3" />
                   </v-col>
                   <v-col cols="6">
-                    <v-btn outlined block @click="login_dialog=!login_dialog">
+                    <v-btn outlined block @click="login_dialog = !login_dialog">
                       Cancel
                     </v-btn>
                   </v-col>
                   <v-col cols="6">
-                    <v-btn color="primary"
-                           type="submit"
-                           :disabled="invalid"
-                           block
-                           :loading="login_loading">
+                    <v-btn
+                      color="primary"
+                      type="submit"
+                      :disabled="invalid"
+                      block
+                      :loading="login_loading"
+                    >
                       Login
                     </v-btn>
                   </v-col>
@@ -86,7 +101,7 @@
 </template>
 
 <script>
-import {ValidationProvider, ValidationObserver} from "vee-validate";
+import { ValidationProvider, ValidationObserver } from "vee-validate";
 import querystring from "querystring";
 
 export default {
@@ -96,111 +111,116 @@ export default {
       login_dialog: false,
       show1: false,
       login_loading: false,
-      identity: '',
-      password: '',
+      identity: "",
+      password: "",
 
-
-      google_login_loading: true
-
-    }
+      google_login_loading: true,
+    };
   },
   components: {
     ValidationProvider,
     ValidationObserver,
   },
-  mounted() {
-
-  },
+  mounted() {},
   watch: {
     login_dialog(val) {
+      console.log("Client id" + process.env.GOOGLE_CLIENT_Id);
       if (val === true) {
         //Initialize google login
         setTimeout(() => {
           window.google.accounts.id.initialize({
             client_id: process.env.GOOGLE_CLIENT_Id,
             callback: this.handleCredentialResponse,
-            auto_select: true
-          })
+            auto_select: true,
+          });
 
-          window.google.accounts.id.renderButton(
-            this.$refs.googleLoginBtn, {
-              text: 'Login',
-              size: 'large',
-              width: '252',
-              theme: 'outline' // option : filled_black | outline | filled_blue
-            }
-          );
+          window.google.accounts.id.renderButton(this.$refs.googleLoginBtn, {
+            text: "Login",
+            size: "large",
+            width: "252",
+            theme: "outline", // option : filled_black | outline | filled_blue
+          });
 
           this.google_login_loading = false;
         }, 4000);
       }
-    }
+    },
   },
   methods: {
     //Handle google login callback
     async handleCredentialResponse(response) {
-      const querystring = require('querystring');
+      const querystring = require("querystring");
 
-      await this.$axios.post('/api/v1/users/googleAuth',
-        querystring.stringify({
-          id_token: response.credential,
-        })).then(response => {
-        this.$auth.setUserToken(response.data.data.jwtToken);
-        this.$auth.setUser(response.data.data.info);
-        this.login_dialog = false;
-        this.$toast.success("Logged in successfully");
+      await this.$axios
+        .post(
+          "/api/v1/users/googleAuth",
+          querystring.stringify({
+            id_token: response.credential,
+          })
+        )
+        .then((response) => {
+          this.$auth.setUserToken(response.data.data.jwtToken);
+          this.$auth.setUser(response.data.data.info);
+          this.login_dialog = false;
+          this.$toast.success("Logged in successfully");
 
-
-        this.$router.push({
-          path: '/user'
+          this.$router.push({
+            path: "/user",
+          });
+        })
+        .catch(({ response }) => {
+          if (response.status == 401) {
+            this.$toast.error(this.$t(`LOGIN_WRONG_DATA`));
+          } else if (response.status == 500 || response.status == 504) {
+            this.$toast.error(this.$t(`REQUEST_FAILED`));
+          }
         });
-      }).catch(({response}) => {
-        if (response.status == 401) {
-          this.$toast.error(this.$t(`LOGIN_WRONG_DATA`));
-        } else if (response.status == 500 || response.status == 504) {
-          this.$toast.error(this.$t(`REQUEST_FAILED`));
-        }
-      });
     },
 
     switchToRegister() {
-      this.$emit("update:switchToRegister", 'register')
+      this.$emit("update:switchToRegister", "register");
     },
     switchToPassRecover() {
-      this.$emit("update:switchToPassRecover", 'pass_recover')
-
+      this.$emit("update:switchToPassRecover", "pass_recover");
     },
     async submit() {
       this.login_loading = true;
-      const querystring = require('querystring');
-      await this.$axios.$post('/api/v1/users/login', querystring.stringify({
-        identity: this.identity,
-        pass: this.password
-      }), {
-        responseType:'text',
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded"
-        }
-      }).then(response => {
-        this.$auth.setUserToken(response.data.jwtToken);
-        this.$auth.setUser(response.data.info);
-        this.login_dialog = false;
-        this.$toast.success("Logged in successfully");
+      const querystring = require("querystring");
+      await this.$axios
+        .$post(
+          "/api/v1/users/login",
+          querystring.stringify({
+            identity: this.identity,
+            pass: this.password,
+          }),
+          {
+            responseType: "text",
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+          }
+        )
+        .then((response) => {
+          this.$auth.setUserToken(response.data.jwtToken);
+          this.$auth.setUser(response.data.info);
+          this.login_dialog = false;
+          this.$toast.success("Logged in successfully");
 
-
-        if (this.$route.path == '/')
-          this.$router.push({
-            path: "/user"
-          });
-      }).catch(err => {
-        if (err.response.status == 400)
-          this.$toast.error(err.response.data.message);
-      }).finally(() => {
-        this.login_loading = false;
-      });
-    }
-  }
-}
+          if (this.$route.path == "/")
+            this.$router.push({
+              path: "/user",
+            });
+        })
+        .catch((err) => {
+          if (err.response.status == 400)
+            this.$toast.error(err.response.data.message);
+        })
+        .finally(() => {
+          this.login_loading = false;
+        });
+    },
+  },
+};
 </script>
 
 <style scoped>
