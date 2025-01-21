c98bd3227644424ef81e3d59d047b7c2d8a72911
Sadeq Naderi <sadeq.naderi@gmail.com>
2025-01-21 16:26:46 +0330

    Cache create question selected option - #Related #54 issue

--- serverMiddleware/data-to-xml.js
+++ serverMiddleware/data-to-xml.js
@@ -64,7 +64,7 @@ async function fetchDataFromServer(contentType) {
   // Fetch data for all pages
   let iterationCount = 0; // Counter to track iterations
 
-  while (iterationCount < 8) {
+  while (iterationCount < 9) {
     let finalUrl = `${apiUrl}&page=${currentPage}&perpage=${itemsPerPage}`;
     if (contentType == "blog")
       finalUrl = `${apiUrl}?page=${currentPage}&perpage=${itemsPerPage}`;
